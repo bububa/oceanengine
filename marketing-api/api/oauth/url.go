e7d1c2781cd39946cf8b9a2cb37626a257fa47c7
Syd Xu <prof.syd.xu@gmail.com>
2021-03-31 16:31:12 +0800

    fix(oauth): change grand_type to grant_type

--- marketing-api/api/oauth/url.go
+++ marketing-api/api/oauth/url.go
@@ -15,5 +15,5 @@ func Url(clt *core.SDKClient, redirectUrl string, state string) string {
 		values.Set("state", state)
 	}
 	values.Set("redirect_uri", redirectUrl)
-	return fmt.Sprintf("%saudit/oauth.html?%s", core.BASE_URL, values.Encode())
+	return fmt.Sprintf("https://ad.oceanengine.com/openapi/audit/oauth.html?%s", values.Encode())
 }
