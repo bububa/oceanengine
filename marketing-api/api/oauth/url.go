11fc215dd73ce16b9690d682a59f6e4742bf9a60
Syd Xu <prof.syd.xu@gmail.com>
2021-08-13 16:51:45 +0800

    feat(oauth): 授权URL中拼接物料授权开关

--- marketing-api/api/oauth/url.go
+++ marketing-api/api/oauth/url.go
@@ -8,12 +8,15 @@ import (
 )
 
 // Url 生成授权链接
-func Url(clt *core.SDKClient, redirectUrl string, state string) string {
+func Url(clt *core.SDKClient, redirectUrl string, state string, materialAuth bool) string {
 	values := &url.Values{}
 	values.Set("app_id", clt.AppID)
 	if state != "" {
 		values.Set("state", state)
 	}
 	values.Set("redirect_uri", redirectUrl)
+	if materialAuth {
+		values.Set("material_auth", "1")
+	}
 	return fmt.Sprintf("https://ad.oceanengine.com/openapi/audit/oauth.html?%s", values.Encode())
 }
