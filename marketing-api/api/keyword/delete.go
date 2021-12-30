d3c764ab5d8c960fa74cf0ee746f7a59b02b6d7d
Syd Xu <prof.syd.xu@gmail.com>
2021-12-30 17:23:44 +0800

    fix(keyword): delete返回结构结构体错误

--- marketing-api/api/keyword/delete.go
+++ marketing-api/api/keyword/delete.go
@@ -7,8 +7,8 @@ import (
 
 // Delete 删除关键词
 // 删除指定keyword_id的搜索词，可批量删除。
-func Delete(clt *core.SDKClient, accessToken string, req *keyword.DeleteRequest) (*keyword.ResponseData, error) {
-	var resp keyword.Response
+func Delete(clt *core.SDKClient, accessToken string, req *keyword.DeleteRequest) (*keyword.DeleteResponseData, error) {
+	var resp keyword.DeleteResponse
 	err := clt.Post("2/keyword/delete_v2/", req, &resp, accessToken)
 	if err != nil {
 		return nil, err
--- marketing-api/model/keyword/delete.go
+++ marketing-api/model/keyword/delete.go
@@ -1,6 +1,10 @@
 package keyword
 
-import "encoding/json"
+import (
+	"encoding/json"
+
+	"github.com/bububa/oceanengine/marketing-api/model"
+)
 
 // DeleteRequest 删除关键词 API Request
 type DeleteRequest struct {
@@ -16,4 +20,19 @@ type DeleteRequest struct {
 func (r DeleteRequest) Encode() []byte {
 	ret, _ := json.Marshal(r)
 	return ret
+}
+
+// DeleteResponse 关键词 API Response
+type DeleteResponse struct {
+	model.BaseResponse
+	// Data json返回值
+	Data *DeleteResponseData `json:"data,omitempty"`
+}
+
+// DeleteResponseData json返回值
+type DeleteResponseData struct {
+	// ErrorList 添加失败的搜索关键词列表
+	ErrorList []Keyword `json:"error_list,omitempty"`
+	// SuccessList 添加成功的搜索关键词列表
+	SuccessList []uint64 `json:"success_list,omitempty"`
 }
