b6a34e49e23dad10a5ca752549537f8700589186
Syd Xu <prof.syd.xu@gmail.com>
2022-03-18 11:54:14 +0800

    fix(creative): component_info missing material_id and component_type

--- marketing-api/model/creative/component_material.go
+++ marketing-api/model/creative/component_material.go
@@ -1,7 +1,13 @@
 package creative
 
+import "github.com/bububa/oceanengine/marketing-api/enum"
+
 // ComponentMaterial 组件信息
 type ComponentMaterial struct {
 	// ComponentID 组件id，通过【查询组件列表】接口获取
 	ComponentID string `json:"component_id,omitempty"`
+	// ComponentType 组件类型
+	ComponentType enum.ComponentType `json:"component_type,omitempty"`
+	// MaterialID
+	MaterialID string `json:"material_id,omitempty"`
 }
