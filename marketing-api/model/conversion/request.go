75d1b6cf1ff1b8be0bfee92d9d46d379cbee8427
Syd Xu <prof.syd.xu@gmail.com>
2022-05-11 18:01:26 +0800

    refactor(conversion): add more properties

--- marketing-api/model/conversion/request.go
+++ marketing-api/model/conversion/request.go
@@ -30,6 +30,8 @@ type Context struct {
 type ContextAd struct {
 	// Callback callback 字段有两个获取途径，对于监测链接归因的方式，需要从监测链接的__CALLBACK_PARAM__这个宏获取这个字段值；对于落地页或小程序归因的方式，需要从 url 中的 clickid 参数获取值。
 	Callback string `json:"callback,omitempty"`
+	// SkuID skuid
+	SkuID string `json:"skuid,omitempty"`
 	// MatchType 这个参数可以帮助广告主标识是通过哪种渠道进行的归因，如果不传，就会默认为点击归因。
 	MatchType enum.ConversionAdMatchType `json:"match_type,omitempty"`
 }
@@ -50,6 +52,110 @@ type Properties struct {
 	PayAmount float64 `json:"pay_amount,omitempty"`
 	// BillTrackType 支付方式
 	BillTrackType enum.ConversionBillTrackType `json:"bill_track_type,omitempty"`
+	// ClassPurchaseAmount 正价课购买次数;背景：教育行业深度转化，优化用户的正价课购买行为；其中有一些用户会一次性产生多科的正价课购买，希望该行为能够表达给深度转化模型优化；释义：用户产生一科正价课购买行为，则属性为1；用户产生3科正价课购买行为，则属性为3；
+	ClassPurchaseAmount int `json:"class_purchase_amount,omitempty"`
+	// PeopleQuality 差异价值人群质量;金融行业授信事件差异价值获取属性1:高质量;2:中质量;3:低质量
+	PeopleQuality int `json:"people_quality,omitempty"`
+	// ValueType 用户价值类型;premium_payment(保险支付)事件下的属性，表示付款用户的用户价值1：低净值2：高净值
+	ValueType int `json:"value_type,omitempty"`
+	// OrderAmount 用户在电商平台APP内，提交订单的金额
+	OrderAmount float64 `json:"order_amount,omitempty"`
+	// ImServiceType 智能客服服务方; 腾讯微信智能客服：wechatim，巨量引擎小六客服：xiaoliuim，抖音私信douyinim，其他待补充腾讯微信智能客服：wechatim，巨量引擎小六客服：xiaoliuim，抖音私信douyinim，其他待补充
+	ImServiceType string `json:"im_service_type,omitempty"`
+	// ReplyMode 智能客服回复方式; auto是系统自动回复，staff是人工客服回复
+	ReplyMode string `json:"reply_mode,omitempty"`
+	// IfHello 智能客服是否发送欢迎语; 1已发送，0已调起客服页面但未触发欢迎语
+	IfHello *int `json:"if_hello,omitempty"`
+	// DialogMode 智能客服对话输入方式; input是用户主动输入,pick是用户点击菜单输入
+	DialogMode string `json:"dialog_mode,omitempty"`
+	// Consumption2 LU搜索侧广告主总消耗金额; LU广告搜索侧广告主的总消耗金额
+	Consumption2 int64 `json:"consumption2,omitempty"`
+	// OriginalEvent 客户自定义的原始事件
+	OriginalEvent string `json:"original_event,omitempty"`
+	// OriginalClassPrice 教育行业客户后链路转化正价课在客户侧的对应课程价格
+	OriginalClassPrice float64 `json:"original_class_price,omitempty"`
+	// OriginalClassID 用户转化的正价课id; 教育行业客户后链路转化正价课在客户侧的对应课程id
+	OriginalClassID string `json:"original_class_id,omitempty"`
+	// M2Surrender M2退保率预估;广告主对保险用户的M2退保率的预估
+	M2Surrender float64 `json:"m2_surrender,omitempty"`
+	// SkuCount 商品数量; 商品数量
+	SkuCount int `json:"sku_count,omitempty"`
+	// SkuAmount 商品单价,单位分
+	SkuAmount int64 `json:"sku_amount,omitempty"`
+	// SkuID 团购商品id
+	SkuID string `json:"sku_id,omitempty"`
+	// AnchorID 主播id
+	AnchorID string `json:"anchor_id,omitempty"`
+	// EnterMethod 直播间来源二级目录
+	EnterMethod string `json:"enter_method,omitempty"`
+	// EnterFromMerge 直播间来源页面
+	EnterFromMerge string `json:"enter_from_merge,omitempty"`
+	// ProductID 商品id
+	ProductID string `json:"product_id,omitempty"`
+	// M2Score M2质量分; 自有建模预估能力的客户对保险用户M2后是否续保的打分
+	M2Score float64 `json:"m2_score,omitempty"`
+	// Consumption LU搜索侧广告主消耗金额; LU广告搜索侧广告主的消耗金额
+	Consumption int64 `json:"consumption,omitempty"`
+	// LengthOfStudy 听课时长; 标示这个转化事件发生时用户传了; 分钟
+	LengthOfStudy int `json:"length_of_study,omitempty"`
+	// Homework 标示这个转化事件发生时用户是否交当堂课作业，0-未交作业，1-交了作业
+	Homework *int `json:"homework,omitempty"`
+	// ClassNum 当前课程节数;标志这个转化事件对应的是教育客户低价课的哪一节
+	ClassNum int `json:"class_num,omitempty"`
+	// FinalClass 是否为末课; 标示这个转化事件对应的是否为教育客户低价课的最后一节，0-不为末课，1-为末课。
+	FinalClass *int `json:"final_class,omitempty"`
+	// OriginClassGrade 用户转化的年级; 教育行业客户转化课程对应的年级
+	OriginClassGrade int `json:"origin_class_grade,omitempty"`
+	// NovalAcLastActiveTs 用户上次在该频道活跃时间戳; 用户上次在该频道活跃时间戳
+	NovalAcLastActiveTs int64 `json:"novel_ac_last_active_ts,omitempty"`
+	// NovalAcCategory 小说频道类型; 用户活跃频道的具体细分
+	NovalAcCategory string `json:"noval_ac_category,omitempty"`
+	// NovalAcGender 小说频道性别类型; 该频道为男频还是女频
+	NovalAcGender *int `json:"noval_ac_gender,omitempty"`
+	// NovalBsAddTs 加入书架时间戳; 加入书架时间戳
+	NovalBsAddTs int64 `json:"noval_bs_add_ts,omitempty"`
+	// NovalBsAuthor 加入书架小说的作者
+	NovalBsAuthor string `json:"noval_bs_author,omitempty"`
+	// NovalBsCategory 加入书架的小说类别
+	NovalBsCategory string `json:"noval_bs_category,omitempty"`
+	// NovalBsTitle 加入书架的小说名
+	NovalBsTitle string `json:"noval_bs_title,omitempty"`
+	// NovalRcLastReadTs 上次阅读时间戳
+	NovalRcLastReadTs int64 `json:"novel_rc_last_read_ts,omitempty"`
+	// NovalRcPaychapter 小说起始付费章节
+	NovalRcPaychapter int `json:"novel_rc_paychapter,omitempty"`
+	// NovalRcDuration 用户在该小说总阅读时长
+	NovalRcDuration int64 `json:"novel_rc_duration,omitempty"`
+	// NovalRcChapter 用户阅读到的小说章节
+	NovalRcChapter int `json:"noval_rc_chapter,omitempty"`
+	// NovalRcAuthor 用户阅读的小说作者
+	NovalRcAuthor string `json:"noval_rc_author,omitempty"`
+	// NovalRcCategory 用户阅读的小说的类别
+	NovalRcCategory string `json:"noval_rc_category,omitempty"`
+	// NovalRcTitle 用户阅读的小说名
+	NovalRcTitle string `json:"noval_rc_title,omitempty"`
+	// NovalType 小说数据行为类型; 回传的是该用户的哪种行为信息
+	NovalType int `json:"noval_type,omitempty"`
+	// LoanValue 贷款价值，互金行业
+	LoanValue float64 `json:"loan_value,omitempty"`
+	// OrderType 订单类型，旅游等行业
+	OrderType int `json:"order_type,omitempty"`
+	// OrderCnt 订单数量，旅游等行业
+	OrderCnt int `json:"order_cnt,omitempty"`
+	// ActiveLevel 激活质量，各行业通用
+	ActiveLevel float64 `json:"active_level,omitempty"`
+	// ReadingTime 阅读时长，小说行业
+	ReadingTime int64 `json:"reading_time,omitempty"`
+	// FromChannel 贷款场景; 互金行业
+	FromChannel int `json:"from_channel,omitempty"`
+	// KeyLevel 关键行为水平
+	KeyLevel float64 `json:"key_level,omitempty"`
+	// PayType 支付方式
+	PayType int `json:"pay_type,omitempty"`
+	// UserValue 用户价值，1-10整数值
+	UserValue int `json:"user_value,omitempty"`
+	// Gender 用户性别
+	Gender int `json:"gender,omitempty"`
 }
 
 // Encode implement GetRequest interface
