c8a542e0a54a2dd9a2abe8d5faf53d64d4906c08
Syd Xu <prof.syd.xu@gmail.com>
2022-06-07 14:29:37 +0800

    feat(creative): 支持字节小程序广告投放链路

---
+++ marketing-api/enum/mini_program_type.go
@@ -0,0 +1,11 @@
+package enum
+
+// MiniProgramType 小程序类型
+type MiniProgramType string
+
+const (
+	// BYTE_GAME 小游戏
+	BYTE_GAME MiniProgramType = "BYTE_GAME"
+	// BYTE_APP 小程序
+	BYTE_APP MiniProgramType = "BYTE_APP"
+)
