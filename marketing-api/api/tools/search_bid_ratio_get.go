ae6b0e3146a3765b01df24a7a059738d8c8c2314
Syd Xu <prof.syd.xu@gmail.com>
2022-08-11 16:39:15 +0800

    feat(tools): 获取快投推荐出价系数

---
+++ marketing-api/api/tools/search_bid_ratio_get.go
@@ -0,0 +1,15 @@
+package tools
+
+import (
+	"github.com/bububa/oceanengine/marketing-api/core"
+	"github.com/bububa/oceanengine/marketing-api/model/tools"
+)
+
+// SearchBidRatioGet 获取快投推荐出价系数
+func SearchBidRatioGet(clt *core.SDKClient, accessToken string, req *tools.SearchBidRatioGetRequest) (float64, error) {
+	var resp tools.SearchBidRatioGetResponse
+	if err := clt.Get("2/tools/search_bid_ratio/get/", req, &resp, accessToken); err != nil {
+		return 0, err
+	}
+	return resp.Data.Ratio, nil
+}
