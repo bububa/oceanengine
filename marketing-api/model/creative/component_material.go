c1c9dfbf76b3d76bb7c3aac031093f55c769e073
Syd Xu <prof.syd.xu@gmail.com>
2022-10-18 13:54:07 +0800

    fix(creative): component_material may string or number

--- marketing-api/model/creative/component_material.go
+++ marketing-api/model/creative/component_material.go
@@ -8,7 +8,7 @@ import (
 // ComponentMaterial 组件信息
 type ComponentMaterial struct {
 	// ComponentID 组件id，通过【查询组件列表】接口获取
-	ComponentID string `json:"component_id,omitempty"`
+	ComponentID model.FlexUint64 `json:"component_id,omitempty"`
 	// ComponentType 组件类型
 	ComponentType enum.ComponentType `json:"component_type,omitempty"`
 	// MaterialID
