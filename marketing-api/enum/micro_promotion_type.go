f5ff68b2d55d83bbf416317a1c7d4515c277b819
Syd Xu <prof.syd.xu@gmail.com>
2023-02-23 13:37:26 +0800

    feat(v3): 创建广告增加用于表达字节小程序信息字段

--- marketing-api/enum/micro_promotion_type.go
+++ marketing-api/enum/micro_promotion_type.go
@@ -6,4 +6,10 @@ type MicroPromotionType string
 const (
 	// MicroPromotionTYpe_WECHAT_GAME 微信小游戏
 	MicroPromotionType_WECHAT_GAME MicroPromotionType = "WECHAT_GAME"
+	// MicroPromotionType_WECHAT_APP 微信小程序
+	MicroPromotionType_WECHAT_APP MicroPromotionType = "WECHAT_APP"
+	// MicroPromotionType_BYTE_GAME 字节小游戏
+	MicroPromotionType_BYTE_GAME MicroPromotionType = "BYTE_GAME"
+	// MicroPromotionType_BYTE_APP 字节小程序
+	MicroPromotionType_BYTE_APP MicroPromotionType = "BYTE_APP"
 )
