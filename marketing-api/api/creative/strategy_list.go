f9669da65cca3bc2b2d0f9bcec1e19b17a6bc68c
Syd Xu <prof.syd.xu@gmail.com>
2023-05-15 11:09:26 +0800

    feat(creative): 增加获取模板（白盒策略）列表 API

---
+++ marketing-api/api/creative/strategy_list.go
@@ -0,0 +1,16 @@
+package creative
+
+import (
+	"github.com/bububa/oceanengine/marketing-api/core"
+	"github.com/bububa/oceanengine/marketing-api/model/creative"
+)
+
+// StrategyList 获取模板（白盒策略）列表
+// 获取白盒策略列表，支持搜索
+func StrategyList(clt *core.SDKClient, accessToken string, req *creative.StrategyListRequest) (*creative.StrategyListData, error) {
+	var resp creative.StrategyListResponse
+	if err := clt.Get("v2/creative/strategy/list/", req, &resp, accessToken); err != nil {
+		return nil, err
+	}
+	return resp.Data, nil
+}
--- marketing-api/model/advertiser/delivery_qualification_list.go
+++ marketing-api/model/advertiser/delivery_qualification_list.go
@@ -35,7 +35,7 @@ func (r DeliveryQualificationListRequest) Encode() string {
 	if r.Page > 1 {
 		values.Set("page", strconv.Itoa(r.Page))
 	}
-	if r.PageSize != 10 {
+	if r.PageSize > 0 {
 		values.Set("page_size", strconv.Itoa(r.PageSize))
 	}
 	ret := values.Encode()
