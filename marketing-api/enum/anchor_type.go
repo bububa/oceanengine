8459450112507ca185dbd934836fec7a7973f50e
Syd Xu <prof.syd.xu@gmail.com>
2023-05-19 11:16:16 +0800

    feat(enum): 补全DeepBidType

--- marketing-api/enum/anchor_type.go
+++ marketing-api/enum/anchor_type.go
@@ -14,4 +14,8 @@ const (
 	AnchorType_ONLINE_SUBSCRIBE AnchorType = "ONLINE_SUBSCRIBE"
 	// AnchorType_SHOPPING_CART 购物车
 	AnchorType_SHOPPING_CART AnchorType = "SHOPPING_CART"
+	// AnchorType_PRIVATE_CHAT  私信锚点
+	AnchorType_PRIVATE_CHAT AnchorType = "PRIVATE_CHAT"
+	// AnchorType_INSURANCE 保险锚点
+	AnchorType_INSURANCE AnchorType = "INSURANCE"
 )
