f70efbff181c6c42bf9ebfb28f4d2bfe5fa5abf3
Syd Xu <prof.syd.xu@gmail.com>
2023-05-22 15:21:22 +0800

    fix(keyword): 升级版获取推荐关键词

--- marketing-api/model/keyword/keyword.go
+++ marketing-api/model/keyword/keyword.go
@@ -18,6 +18,8 @@ type Keyword struct {
 	KeywordID uint64 `json:"keyword_id,omitempty"`
 	// WordID 词ID，不同计划下如果关键词的内容相同，词id会相同
 	WordID uint64 `json:"word_id,omitempty"`
+	// Msv 月搜索量
+	Msg int64 `json:"msv,omitempty"`
 	// ErrorReason 关键词添加失败原因
 	ErrorReason string `json:"error_reason,omitempty"`
 }
