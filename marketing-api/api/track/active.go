e7da2419bca74ef66847d7c4e0e5c05d4490aa74
Syd Xu <prof.syd.xu@gmail.com>
2023-07-13 13:42:33 +0800

    refactor(track): 转化回传服务支持鉴权

--- marketing-api/api/track/active.go
+++ marketing-api/api/track/active.go
@@ -1,63 +1,15 @@
 package track
 
 import (
-	"encoding/json"
-	"net/http"
-	"strconv"
-
-	"github.com/bububa/oceanengine/marketing-api/enum"
+	"github.com/bububa/oceanengine/marketing-api/core"
 	"github.com/bububa/oceanengine/marketing-api/model/track"
-	"github.com/bububa/oceanengine/marketing-api/util"
 )
 
 // Active API上报数据(new)
-func Active(req *track.ActiveRequest) (string, error) {
-	values := util.GetUrlValues()
-	if req.Callback != "" {
-		values.Set("callback", req.Callback)
-		values.Set("os", strconv.Itoa(req.Os))
-		if req.Os == enum.Track_ANDROID {
-			values.Set("imei", req.Imei)
-			values.Set("muid", req.Muid)
-			values.Set("oaid", req.Oaid)
-			if req.OaidMd5 != "" {
-				values.Set("oaid_md5", req.OaidMd5)
-			}
-		} else {
-			values.Set("idfa", req.Idfa)
-		}
-		if req.Caid1 != "" {
-			values.Set("caid1", req.Caid1)
-		}
-		if req.Caid2 != "" {
-			values.Set("caid2", req.Caid2)
-		}
-	} else {
-		values.Set("link", req.Link)
+func Active(clt *core.SDKClient, req *track.ActiveRequest) (int, error) {
+	var resp track.Response
+	if err := clt.TrackActive(req, &resp); err != nil && resp.Code != 128 {
+		return resp.Code, err
 	}
-	values.Set("event_type", strconv.Itoa(req.EventType))
-	if req.ConvTime > 0 {
-		values.Set("conv_time", strconv.FormatInt(req.ConvTime, 10))
-	}
-	if req.Source != "" {
-		values.Set("source", req.Source)
-	}
-	for k, v := range req.Ext {
-		values.Set(k, v)
-	}
-	reqUrl := util.StringsJoin("https://ad.oceanengine.com/track/activate/?", values.Encode())
-	util.PutUrlValues(values)
-	resp, err := http.DefaultClient.Get(reqUrl)
-	if err != nil {
-		return reqUrl, err
-	}
-	defer resp.Body.Close()
-	var ret track.Response
-	if err := json.NewDecoder(resp.Body).Decode(&ret); err != nil {
-		return reqUrl, err
-	}
-	if ret.IsError() {
-		return reqUrl, ret
-	}
-	return reqUrl, nil
+	return resp.Code, nil
 }
