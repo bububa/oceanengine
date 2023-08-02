8d263eda930f00f33731e88d508ec3c58b89b8af
Syd Xu <prof.syd.xu@gmail.com>
2023-08-02 11:26:30 +0800

    feat(tools/sitetemplate): 建站管理-橙子建站模板管理模块新增「获取站点url」接口，支持获取模板及站点下的url

---
+++ marketing-api/api/tools/sitetemplate/pic_url_get.go
@@ -0,0 +1,16 @@
+package sitetemplate
+
+import (
+	"github.com/bububa/oceanengine/marketing-api/core"
+	"github.com/bububa/oceanengine/marketing-api/model/tools/sitetemplate"
+)
+
+// PicURLGet 获取模板/站点URL
+// 通过 site_id / template_id 获取站点/模板下的图片加签URL
+func PicURLGet(clt *core.SDKClient, accessToken string, req *sitetemplate.PicURLGetRequest) (map[string]string, error) {
+	var resp sitetemplate.PicURLGetResponse
+	if err := clt.Get("2/tools/site_template/pic_url/get/", req, &resp, accessToken); err != nil {
+		return nil, err
+	}
+	return resp.Data.URLMap, nil
+}
