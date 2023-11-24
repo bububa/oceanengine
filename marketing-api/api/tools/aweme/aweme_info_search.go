5c67edfb52b20d31318a0a7f45cbed406e3e43e3
Syd Xu <prof.syd.xu@gmail.com>
2023-11-24 18:22:11 +0800

    fix(tools/aweme): aweme_info_search response

--- marketing-api/api/tools/aweme/aweme_info_search.go
+++ marketing-api/api/tools/aweme/aweme_info_search.go
@@ -6,7 +6,7 @@ import (
 )
 
 // AwemeInfoSearch 查询抖音帐号和类目信息
-func AwemeInfoSearch(clt *core.SDKClient, accessToken string, req *aweme.AwemeInfoSearchRequest) (*aweme.AwemeInfoSearchResult, error) {
+func AwemeInfoSearch(clt *core.SDKClient, accessToken string, req *aweme.AwemeInfoSearchRequest) ([]aweme.AwemeInfoSearchResult, error) {
 	var resp aweme.AwemeInfoSearchResponse
 	err := clt.Get("2/tools/aweme_info_search/", req, &resp, accessToken)
 	if err != nil {
