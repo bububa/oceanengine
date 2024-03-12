32936d10f2437df017871e950f5b2de4db0829e2
Syd Xu <prof.syd.xu@gmail.com>
2024-03-12 13:43:23 +0800

    feat(file): 新接口上线，支持代理商将自产素材上传到巨量方舟平台

--- marketing-api/api/file/rebate_material_search.go
+++ marketing-api/api/file/rebate_material_search.go
@@ -5,7 +5,8 @@ import (
 	"github.com/bububa/oceanengine/marketing-api/model/file"
 )
 
-// RebateMaterialSearch 获取低效素材List
+// RebateMaterialSearch 【代理商】明点无效素材查询
+// 支持根据代理商id和月份日期等入参数，查找返点政策粒度下的有效/无效素材
 func RebateMaterialSearch(clt *core.SDKClient, accessToken string, req *file.RebateMaterialSearchRequest) (*file.RebateMaterialSearchResult, error) {
 	var resp file.RebateMaterialSearchResponse
 	if err := clt.Get("2/file/rebate/material_search/", req, &resp, accessToken); err != nil {
