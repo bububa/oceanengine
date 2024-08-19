aedf9d9d53818feb272c214b43a9a9480ab17ff5
Syd Xu <prof.syd.xu@gmail.com>
2024-08-19 13:30:28 +0800

    qianchuan(report): 获取直播大屏数据相关接口升级，历史接口将于2024.9.17号下线

--- marketing-api/api/qianchuan/report/long_transfer_order_data_get.go
+++ marketing-api/api/qianchuan/report/long_transfer_order_data_get.go
@@ -6,7 +6,7 @@ import (
 )
 
 // LongTransferOrderDataGet 获取长周期订单数据
-func LongTransferOrderDataGet(clt *core.SDKClient, accessToken string, req *report.LongTransferOrderDataGetRequest) (*report.CustomGetResult, error) {
+func LongTransferOrderDataGet(clt *core.SDKClient, accessToken string, req *report.CustomGetRequest) (*report.CustomGetResult, error) {
 	var resp report.CustomGetResponse
 	err := clt.Get("v1.0/qianchuan/report/long_transfer/order/data/get/", req, &resp, accessToken)
 	if err != nil {
