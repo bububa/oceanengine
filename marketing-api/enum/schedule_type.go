3b232044c7dcc814c8a72b21c350fd8cca8e3c7e
Syd Xu <prof.syd.xu@gmail.com>
2024-09-20 16:42:37 +0800

    feat(project): delivery_setting.schedule_type 投放时间类型新增SCHEDULE_7_DAYS 7日稳投

--- marketing-api/enum/schedule_type.go
+++ marketing-api/enum/schedule_type.go
@@ -4,6 +4,8 @@ package enum
 type ScheduleType string
 
 const (
+	// SCHEDULE_7_DAYS 7日稳投 【新增】
+	SCHEDULE_7_DAYS ScheduleType = "SCHEDULE_7_DAYS"
 	// SCHEDULE_FROM_NOW 从现在开始一直投放
 	SCHEDULE_FROM_NOW ScheduleType = "SCHEDULE_FROM_NOW"
 	// SCHEDULE_START_END 选择起始时间
