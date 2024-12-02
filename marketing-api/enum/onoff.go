d898cdbee9c8ac732e207064dd6ba8115242c3b0
Syd Xu <prof.syd.xu@gmail.com>
2024-12-02 13:35:46 +0800

    fix(enum): OnOff unmarshal

--- marketing-api/enum/onoff.go
+++ marketing-api/enum/onoff.go
@@ -1,5 +1,7 @@
 package enum
 
+import "strconv"
+
 type OnOff string
 
 const (
@@ -8,3 +10,28 @@ const (
 	// OFF 关闭
 	OFF OnOff = "OFF"
 )
+
+func (ooi *OnOff) UnmarshalJSON(b []byte) (err error) {
+	if b[0] == '"' && b[len(b)-1] == '"' {
+		b = b[1 : len(b)-1]
+	}
+	str := string(b)
+	var v string
+	switch str {
+	case "ON", "OFF":
+		v = str
+	default:
+		if str != "" {
+			if i, err := strconv.Atoi(str); err == nil {
+				switch i {
+				case 1:
+					v = "ON"
+				case 0:
+					v = "OFF"
+				}
+			}
+		}
+	}
+	*ooi = OnOff(v)
+	return nil
+}
