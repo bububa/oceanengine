483e00220c92d03c0efdd64e00194e8b5ee5bbce
Syd Xu <prof.syd.xu@gmail.com>
2025-11-05 12:28:44 +0800

    feat(qianchuan/uni_promotion): 千川随心推全域投放新增支持订单&素材维度数据获取能力

--- marketing-api/model/qianchuan/uni_promotion/authorized_get.go
+++ marketing-api/model/qianchuan/uni_promotion/authorized_get.go
@@ -3,6 +3,7 @@ package unipromotion
 import (
 	"strconv"
 
+	"github.com/bububa/oceanengine/marketing-api/enum"
 	"github.com/bububa/oceanengine/marketing-api/model"
 	"github.com/bububa/oceanengine/marketing-api/model/qianchuan/aweme"
 	"github.com/bububa/oceanengine/marketing-api/util"
@@ -12,12 +13,27 @@ import (
 type AuthorizedGetRequest struct {
 	// AdvertiserID 广告主id
 	AdvertiserID uint64 `json:"advertiser_id,omitempty"`
+	// Filtering 过滤器
+	Filtering *AuthorizedGetFilter `json:"filtering,omitempty"`
 	// Page 页码
 	// 默认值： 1
 	Page int `json:"page,omitempty"`
 	// PageSize 页面大小
 	// 默认值：10，最大值：100
 	PageSize int `json:"page_size,omitempty"`
+}
+
+type AuthorizedGetFilter struct {
+	// MarketingGoal 营销目标，可选值:
+	// LIVE_PROM_GOODS 直播全域（默认）
+	// VIDEO_PROM_GOODS 商品全域
+	MarketingGoal enum.MarketingGoal `json:"marketing_goal,omitempty"`
+	// SearchKeyWords 检索词，支持根据抖音号show_Id/抖音号名搜索
+	SearchKeyWords string `json:"search_key_words,omitempty"`
+	// Scene 查询场景，可选值:
+	// CREATE 计划创编页面（默认）
+	// APPLY 申请授权页面
+	Scene string `json:"scene,omitempty"`
 }
 
 // Encode implement GetRequest interface
